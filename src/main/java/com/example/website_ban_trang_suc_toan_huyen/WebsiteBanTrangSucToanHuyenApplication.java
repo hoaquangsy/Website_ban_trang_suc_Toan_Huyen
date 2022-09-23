d2300fa9a98f107300a9768aec52734822e66d33
hoaquangsy <77135777+hoaquangsy@users.noreply.github.com>
2022-09-23 22:53:45 +0700

    baseProject

---
+++ src/main/java/com/example/website_ban_trang_suc_toan_huyen/WebsiteBanTrangSucToanHuyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.website_ban_trang_suc_toan_huyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBanTrangSucToanHuyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBanTrangSucToanHuyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/website_ban_trang_suc_toan_huyen/WebsiteBanTrangSucToanHuyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.website_ban_trang_suc_toan_huyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBanTrangSucToanHuyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
