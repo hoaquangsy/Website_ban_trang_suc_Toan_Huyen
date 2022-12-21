74ce8a1213ce7ec8b9ad1afd824d4866f9e0f04b
hoaquangsy <77135777+hoaquangsy@users.noreply.github.com>
2022-12-21 22:21:05 +0700

    fix

--- src/main/java/com/example/website_ban_trang_suc_toan_huyen/repository/OrderDetailRepository.java
+++ src/main/java/com/example/website_ban_trang_suc_toan_huyen/repository/OrderDetailRepository.java
@@ -18,8 +18,8 @@ public interface OrderDetailRepository extends JpaRepository<OrderDetailEntity, 
     @Query(" select o from OrderDetailEntity o where o.orderId = :orderId and o.productId in :productId")
     List<OrderDetailEntity> findByOrderIdAndProductIdIn(UUID orderId, List<UUID> productId);
 
-    @Query(value = "SELECT * FROM `order_detail` GROUP BY order_id \n" +
-            "ORDER BY COUNT(quantity)",nativeQuery = true)
+    @Query(value = "SELECT * FROM `order_detail` dt JOIN Orders o ON o.id=dt.order_id WHERE o.status='DA_GIAO' GROUP BY dt.order_id,dt.product_id \n" +
+            "ORDER BY COUNT(dt.quantity) ",nativeQuery = true)
     List<OrderDetailEntity> findByProductTrending();
 
      Optional<OrderDetailEntity> findByOrderIdAndProductIdAndSizeId(UUID orderId, UUID productId,UUID sizeId);
